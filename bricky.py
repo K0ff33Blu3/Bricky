7dab508b91d0b88750aa6122253cc1ed5659263d
k0ff33Blu3 <milva.ricci@gmail.com>
2026-03-26 09:32:57 +0100

    cnreui

--- bricky.py
+++ bricky.py
@@ -1,13 +1,23 @@
 import math
 import pygame
 
+class	Point(pygame.sprite.Sprite) :
+	def __init__(self) :
+		super().__init__()
+		self.width = 40
+		self.height = 10
+		self.image = pygame.Surface((40, 10))
+		self.image.fill((255, 0, 0))
+		self.rect = self.image.get_rect()
+		self.font = pygame.font.SysFont()
+
 class	Ball(pygame.sprite.Sprite) :
 	def __init__(self) :
 		super().__init__()
 		self.image = pygame.Surface((20, 20), pygame.SRCALPHA)
-		pygame.draw.circle(self.image, (0,0,0), (5,5), 5)
+		pygame.draw.circle(self.image, (255,0,0), (5,5), 5)
 		self.rect = self.image.get_rect()
-		self.rect.center = ((300, 10))
+		self.rect.center = ((300, 400))
 		self.center_pos= pygame.math.Vector2(self.rect.center)
 		self.speed = pygame.math.Vector2(0, 10)
 
@@ -24,13 +34,19 @@ class	Ball(pygame.sprite.Sprite) :
 			self.speed.y *= -1
 			self.speed = self.speed.normalize() * 10
 		return self.rect.bottom < 800
+	
+	def	bounce(self, spr : pygame.sprite.Sprite) :
+		self.speed.y *= -1
+		hitpoint = (self.center_pos.x - spr.center_pos_x) / 50
+		self.speed.x = hitpoint * 10
+		self.speed = self.speed.normalize() * 10
 
 class	Platform(pygame.sprite.Sprite) :
 	SPEED = 10
 	def __init__(self) :
 		super().__init__()
 		self.image = pygame.Surface((100, 10))
-		self.image.fill((0,0,0))
+		self.image.fill((255,255,255))
 		self.rect = self.image.get_rect()
 		self.rect.center = ((300, 700))
 		self.center_pos_x = self.rect.centerx
@@ -48,13 +64,15 @@ class	Platform(pygame.sprite.Sprite) :
 		self.rect.centerx = int(self.center_pos_x)
 
 class	Brick(pygame.sprite.Sprite) :
-	def __init__(self, topleft) :
+	def __init__(self, topleft, color) :
 		super().__init__()
 		self.image = pygame.Surface((60,30))
-		self.image.fill((148, 251, 171))
+		self.image.fill(color)
 		self.rect = self.image.get_rect()
-		pygame.draw.rect(self.image, (0,0,0), self.rect, 2)
+		pygame.draw.rect(self.image, color, self.rect, 2)
 		self.rect.topleft = topleft
+		self.center_pos_x = self.rect.centerx
+
 	
 # init library
 pygame.init()
@@ -62,7 +80,6 @@ pygame.init()
 # init display
 win = pygame.display.set_mode((600, 800))
 pygame.display.set_caption("Bricky")
-win.fill((232, 174, 183))
 
 # init clock
 clock = pygame.time.Clock()
@@ -71,24 +88,38 @@ FPS = 60
 # init sprites 
 platform = Platform()
 ball = Ball()
-all_sprites = pygame.sprite.Group()
-all_sprites.add(platform, ball)
+
 # init bricks 
 bricks = []
+red = 0
+green = 242
+blue = 289
+for y in range(0, 5) :
+	red += 48
+	blue -= 48
+	color = (red, green, blue)
+	for x in range(0,11) :
+		bricks.append(Brick((x * 60, y * 30), color))
+bricks_sprites = pygame.sprite.Group()
+bricks_sprites.add(bricks)
 
+all_sprites = pygame.sprite.Group()
+all_sprites.add(platform, ball, bricks)
 
 # game loop 
 running = True
 while running :
 	# set sprites
-	win.fill((232, 174, 183))
+	win.fill((0, 0, 0))
 	running = ball.move()
 
 	# handle collisions
 	if ball.rect.colliderect(platform.rect) :
-		ball.speed.y *= -1
-		hitpoint = (ball.center_pos.x - platform.center_pos_x) / 50
-		ball.speed.x = hitpoint * 10
+		ball.bounce(platform)
+
+	hitbrick = pygame.sprite.spritecollide(ball, bricks_sprites, True)
+	if  hitbrick :
+		ball.bounce(hitbrick[0])
 
 	all_sprites.draw(win)
 	pygame.display.flip()
@@ -108,3 +139,5 @@ while running :
 	for event in pygame.event.get() :
 		if event.type == pygame.QUIT :
 			running = False
+			exit()
+
